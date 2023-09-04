9dfb367efd5489f5e5611d63bc34afe1fd3eae7c
Shiki <pitatensai@163.com>
2023-09-04 13:45:45 +0800

    init

---
+++ lib/gcc/arm-unknown-linux-gnueabihf/8.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/xtool-build-env/.build/arm-unknown-linux-gnueabihf/src/gcc/configure --build=x86_64-build_apple-darwin18.7.0 --host=x86_64-build_apple-darwin18.7.0 --target=arm-unknown-linux-gnueabihf --prefix=/Volumes/xtool-build-env/arm-unknown-linux-gnueabihf --with-sysroot=/Volumes/xtool-build-env/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot --enable-languages=c,c++ --with-cpu=cortex-a9 --with-fpu=neon-vfpv4 --with-float=hard --with-pkgversion='crosstool-NG 1.24.0' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/Volumes/xtool-build-env/.build/arm-unknown-linux-gnueabihf/buildtools --with-mpfr=/Volumes/xtool-build-env/.build/arm-unknown-linux-gnueabihf/buildtools --with-mpc=/Volumes/xtool-build-env/.build/arm-unknown-linux-gnueabihf/buildtools --with-isl=/Volumes/xtool-build-env/.build/arm-unknown-linux-gnueabihf/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/Volumes/xtool-build-env/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a9" }, { "float", "hard" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
